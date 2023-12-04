703e9a5571d5748d27bcd5bd68beda44cedde9ad
Jing Jing Zhang <92764807+jjzhang329@users.noreply.github.com>
2023-12-04 15:31:25 -0600

    Create linked_list_cycleII.py

---
+++ Linked_List/linked_list_cycleII.py
@@ -0,0 +1,42 @@
+# Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+# Do not modify the linked list.
+# 142. Linked List Cycle II
+# https://leetcode.com/problems/linked-list-cycle-ii/description/
+
+# Approach: Floyd's cycle detection - Tortoise and Hare algo
+# fast and slow pointer, fast will move 2x speed than slow
+# if fast == slow, cycle detected
+# once cycle detected, move the slow pointer to head, and 
+# fast stay at intersection, now both pointer move 1 step
+# they will meet at the cycle point.
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            # If there is a cycle, the slow and fast pointers will eventually meet
+            if slow == fast:
+                # Move one of the pointers back to the start of the list
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        # If there is no cycle, return None
+        return None
+# Input: head = [3,2,0,-4], pos = 1
+# Output: tail connects to node index 1
+# Explanation: There is a cycle in the linked list, where tail connects to the second node.
